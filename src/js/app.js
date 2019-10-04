2ea8d215dab862942739edbd25cadf1391c4c4a7
Dmitry Teslitsky <dmitryteslitsky@gmail.com>
2019-10-04 17:08:00 +0300

    Fix location coordinates

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 /* global $, google, AOS */
 
 function initializeMap() {
-  const myLatLng = new google.maps.LatLng(50.466109, 30.5051482);
+  const myLatLng = new google.maps.LatLng(50.4664452, 30.50814);
   const mapOptions = {
     zoom: 18,
     center: myLatLng,
